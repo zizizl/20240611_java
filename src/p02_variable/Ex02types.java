4a67c90d576eef26a5aa199106c7315a8ee775d3
zizizl <2tpuiop987@naver.com>
2024-06-11 16:11:25 +0900

    init

--- src/p02_variable/Ex02types.java
+++ src/p02_variable/Ex02types.java
@@ -2,6 +2,15 @@ package p02_variable;
 
 public class Ex02types {
   public static void main(String[] args) {
-
+    //변수의 종류 크게: Primitive, Reference
+    // Primitive type( 일반 변수는 실제 값을 가짐 )
+    boolean power = true; // 논리 타입 - true, false 값을 나타냄, 1byte
+    char c1 = 'A';        // 문자 타입 - 2byte, 0~65535
+    byte b1 = 18;
+    short s1 = 10;
+    int i1 = 10;
+    long l1 = 10L;
+    float f1 = 0.1f;
+    double d1 = 0.1d;
   }
 }
