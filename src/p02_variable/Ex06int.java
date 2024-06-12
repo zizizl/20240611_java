9c51e4ebd7ebd8c8ce0b94754175eec04fdb40b1
zizizl <2tpuiop987@naver.com>
2024-06-12 16:26:49 +0900

    init

--- src/p02_variable/Ex06int.java
+++ src/p02_variable/Ex06int.java
@@ -12,16 +12,19 @@ public class Ex06int {
     byte b4 = (byte) (b1 + b3);
     Utils.typeOf(b4);
     System.out.println(b4);
-// boolean 제외한 4byte 이하 자료형은 int로 변환후 연산
+// 1. boolean 제외한 4byte 이하 자료형은 int로 변환후 연산
     short s1 = 1000;
     short s2 = 1000;
+
+// 2. 표현 범위가 큰것이 작은 것으로 올 때는 명시적 형변환
     short s3 = (short) (s1 + s2);
     System.out.println(s3);
     int i1 = s1 + s2;
     System.out.println(i1);
 
+// 3. 표현 범위가 작은 것이 큰 것으로 올 때는 묵시적 형변환
     long l1 = 200l; // long = long
-    long l2 = 200;  // long = int   longl2 = (long)200 이렇게 표현한건데 long이 생략되서 표시됨. 묵시적 형변환
+    long l2 = 200;  // long = int   longl2 = (long)200 이렇게 표현한건데 long이 생략되서 표시됨.
 
 
   }
