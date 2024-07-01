9cc758d9fbc346021fb400334c8f175fd7d2a572
zizizl <2tpuiop987@naver.com>
2024-07-01 11:51:27 +0900

    init

--- src/p07_Collection/Ex11GenericsSuperExtends.java
+++ src/p07_Collection/Ex11GenericsSuperExtends.java
@@ -7,14 +7,15 @@ public class Ex11GenericsSuperExtends {
     Gen3 gen3 = new Gen3();
     // <? super type>, <? extends type> 상속관계를 이용하여 타입을 제약
 
-    // Gen2의 조상이 Gen1, Gen1을 조상으로 하는 하위 모든 자손 gen1, gen2, gen3
+    // Gen2의 조상이 Gen1, Gen1을 조상으로 하는 하위 모든 자손 gen1,gen2,gen3
     GenType<? super Gen2> genType = new GenType<>(gen3);
 
     // Gen2를 상속받는 하위 모든 자손 gen2, gen3
     GenType<? extends Gen2> genType2 = new GenType<>(gen2);
 
-    // ?는 모든 클래스 타입이 가능
+    // ?는 모든 크래스 타입이 가능
     GenType<?> genType3 = new GenType<>(new Object());
+
   }
 }
 
@@ -28,10 +29,18 @@ class Gen1 {
 
 class Gen2 extends Gen1 {
   String name = "Gen2";
+
+  public String toString() {
+    return name;
+  }
 }
 
 class Gen3 extends Gen2 {
-  String name = "Gen2";
+  String name = "Gen3";
+
+  public String toString() {
+    return name;
+  }
 }
 
 class GenType<T> {
